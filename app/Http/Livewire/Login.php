be4d53e1d660322ac9ec8ea92f3c8682ef523c2e
Nuh Mirsada <nosada29@gmail.com>
2020-09-22 15:48:16 +0700

    buat login

---
+++ app/Http/Livewire/Login.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class Login extends Component
+{
+    public $users, $email, $password, $name;
+
+    public function render()
+    {
+        return view('livewire.login');
+    }
+
+    private function resetInputField(){
+        $this->name = '';
+        $this->email = '';
+        $this->password = '';
+    }
+
+    public function login()
+    {
+        $this->validatedDate = $this->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt(['email' => $this->email, 'password' => $this->password])){
+            session()->flash('message', "You are Login successful.");
+            return redirect()->to('/home');
+        }else{
+            session()->flash('error', 'email and password are wrong.');
+        }
+    }
+
+
+
+
+}
