d4686cb1350fae1bbcb5db5ea79bfe00fe95f6a4
Nuh Mirsada <nosada29@gmail.com>
2020-10-18 16:20:41 +0700

    edit readme & favicon

--- .history/resources/views/layouts/app.blade_20201018153332.php
+++ .history/resources/views/layouts/app.blade_20201018153332.php
@@ -12,7 +12,6 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link rel="icon" href="{{ url('images/logo_small.ico') }}" type="image/x-icon">
     <!-- Styles -->
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
